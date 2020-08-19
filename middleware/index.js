2d4acf9b7b679a9fe75f30357a80f1923b450074
sitcomtechnology <sitcomtechnology5@gmail.com>
2020-08-19 19:32:28 +0530

    removed console

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,5 @@
 function Auth(req, res, next) {
-    console.log("1", req.session)
-    console.log("2", req.session.user)
     if (req.session.user) {
-        //req.isAuthenticated() will return true if user is logged in
         next();
     } else {
         res.status(401).send({ message: "Please login" })
